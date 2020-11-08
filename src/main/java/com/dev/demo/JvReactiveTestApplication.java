a5ac4fede47e489d79af28db7306442df28ee6a0
Alexey <gradiviro@gmail.com>
2020-11-08 22:17:51 +0200

    first commit

---
+++ src/main/java/com/dev/demo/JvReactiveTestApplication.java
@@ -0,0 +1,13 @@
+package com.dev.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvReactiveTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvReactiveTestApplication.class, args);
+	}
+
+}
